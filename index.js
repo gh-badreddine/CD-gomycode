9c0ef4043273f661463add110543aff264cce0d9
gh-badreddine <30468357+gh-badreddine@users.noreply.github.com>
2020-02-16 12:31:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const http = require('http')
 const PORT = process.env.PORT || 8080
 http
   .createServer((req, res) => {
-    res.end('Hello GOMYCODE v1.1');
+    res.end('Hello GOMYCODE v1.0');
   })
   .listen(PORT, () => console.log('Listening on', PORT))
